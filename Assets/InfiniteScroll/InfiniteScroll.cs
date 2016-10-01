3744ca547b2f51dddf424f0af0eeb4fe554f008e
gr2day <pepermint113@gmail.com>
2016-10-01 11:39:45 +0900

    First Commint

---
+++ Assets/InfiniteScroll/InfiniteScroll.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+
+public class InfiniteScroll : MonoBehaviour
+{
+	[SerializeField] private RectTransform		m_Content;
+	[SerializeField] private RectTransform[] 	m_PoolItems;
+	[SerializeField] private bool				m_IsVertical;
+
+	private Action<int, int> 					m_CallbackChanged;
+	private Vector2 							m_ItemSize;
+
+	private int									m_DataCount;
+	private int									m_PoolItemCount;
+	private int 								m_MoveIndex;
+
+	public void Init( int dataCount, Action<int, int> callbackChanged )
+	{
+		m_PoolItemCount = m_PoolItems.Length;
+
+		if( m_PoolItemCount == 0 ) return;
+
+		m_CallbackChanged = callbackChanged;
+
+		m_MoveIndex = 0;
+		m_DataCount = dataCount;
+		m_ItemSize = m_PoolItems[0].sizeDelta;
+
+		for( int i = 0; i < m_PoolItems.Length; i++ ) 
+		{
+			SetPositionPoolItem( i, i );
+			if( m_CallbackChanged != null ) m_CallbackChanged( i, i );
+		}
+
+		SetContentSize();
+	}
+
+	void Update()
+	{
+		if( m_PoolItemCount == 0 ) return;
+
+		float itemSize 			= GetItemSize();
+		float contentPosition 	= GetContentPosition();
+
+		while( itemSize * ( m_MoveIndex + 2 ) < contentPosition )
+		{
+			if( m_MoveIndex >= m_DataCount - m_PoolItemCount ) return;
+
+			int poolIndex = m_MoveIndex % m_PoolItemCount;
+			int dataIndex = m_PoolItemCount + m_MoveIndex;
+
+			SetPositionPoolItem( poolIndex, dataIndex );
+			m_MoveIndex++;
+
+			if( m_CallbackChanged != null )
+			{
+				m_CallbackChanged( poolIndex, dataIndex );
+			}
+		}
+
+		while( itemSize * ( m_MoveIndex + 1 ) > contentPosition )
+		{
+			if( m_MoveIndex == 0 ) return;
+			m_MoveIndex--;
+
+			int poolIndex = m_MoveIndex % m_PoolItemCount;
+			int dataIndex =	m_MoveIndex;
+
+			SetPositionPoolItem( poolIndex, dataIndex );
+
+			if( m_CallbackChanged != null ) 
+			{
+				m_CallbackChanged( poolIndex, dataIndex );
+			}
+		}
+	}
+
+	private void SetContentSize()
+	{
+		Vector2 contentSize = m_Content.sizeDelta;
+
+		if( m_IsVertical )
+		{
+			contentSize.y = m_ItemSize.y * (float)m_DataCount;
+		}
+		else
+		{
+			contentSize.x = m_ItemSize.x * (float)m_DataCount;
+		}
+
+		m_Content.sizeDelta = contentSize;
+	}
+
+	private float GetItemSize()
+	{
+		if( m_IsVertical )
+		{
+			return m_ItemSize.y;
+		}
+
+		return m_ItemSize.x;
+	}
+
+	private float GetContentPosition()
+	{
+		if( m_IsVertical )
+		{
+			return m_Content.anchoredPosition.y;
+		}
+
+		return -m_Content.anchoredPosition.x;
+	}
+
+	private void SetPositionPoolItem( int poolIndex, int dataIndex )
+	{
+		if( m_IsVertical ) 
+		{
+			m_PoolItems[ poolIndex ].anchoredPosition = new Vector2( 0.0f, -GetItemSize() * dataIndex );
+		}
+		else
+		{
+			m_PoolItems[ poolIndex ].anchoredPosition = new Vector2( GetItemSize() * dataIndex, 0.0f );
+		}
+	}
+
+	public GameObject GetPoolItem( int index )
+	{
+		return m_PoolItems[ index ].gameObject;
+	}
+}
---
+++ Assets/Sample/InfiniteScrollScene.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InfiniteScrollScene : MonoBehaviour
+{
+	private readonly string[] DATA_LIST = new string[]
+	{
+		"Item01", "Item02", "Item03", "Item04", "Item05",
+		"Item06", "Item07", "Item08", "Item09", "Item10",
+		"Item11", "Item12", "Item13", "Item14", "Item15",
+		"Item16", "Item17", "Item18", "Item19", "Item20",
+	};
+
+	[SerializeField] private InfiniteScroll m_VerticalInfiniteScroll;
+	[SerializeField] private InfiniteScroll m_HorizontalInfiniteScroll;
+
+	// Use this for initialization
+	void Start () 
+	{
+		m_VerticalInfiniteScroll.Init( 
+			DATA_LIST.Length, 
+			delegate( int poolIndex, int dataIndex )
+			{
+				Item item = m_VerticalInfiniteScroll.GetPoolItem( poolIndex ).GetComponent<Item>();
+				item.Init( DATA_LIST[dataIndex] );
+			}
+		);
+
+		m_HorizontalInfiniteScroll.Init( 
+			DATA_LIST.Length, 
+			delegate( int poolIndex, int dataIndex )
+			{
+				Item item = m_HorizontalInfiniteScroll.GetPoolItem( poolIndex ).GetComponent<Item>();
+				item.Init( DATA_LIST[dataIndex] );
+			}
+		);
+	}
+}
